998a3f0a4b7a9c0940e24580979a94701a8f5bf2
Andrew Peattie <drewpeattie@hotmail.com>
2020-08-31 15:21:32 +0100

    random colors

--- RGB.js
+++ RGB.js
@@ -8,24 +8,37 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
     // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
-
     // add click listeners to squares
     squares[i].addEventListener("click", function() {
     // grab color of clicked square
     var clickedColor = this.style.backgroundColor
     // compare color to pickedColor
     if(clickedColor === pickedColor) {
-        alert("correct");
+        messageDisplay.textContent = "Correct";
+        changeColors(clickedColor);
     } else {
-        alert("wrong");
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again"
     }
     });
+}
+
+function changeColors(color) {
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+   var random = Math.floor(Math.random() * colors.length)
+    return colors[random];
 }
