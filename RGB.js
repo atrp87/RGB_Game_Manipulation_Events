26f6287aa8e8ff86d37f53d42f87debf16293c08
Andrew Peattie <drewpeattie@hotmail.com>
2020-09-01 13:43:59 +0100

    stripe style

--- RGB.js
+++ RGB.js
@@ -65,13 +65,13 @@ for(var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor
     // compare color to pickedColor
     if(clickedColor === pickedColor) {
-        messageDisplay.textContent = "Correct";
+        messageDisplay.textContent = "Correct.";
         resetButton.textContent = "Play Again"
         changeColors(clickedColor);
         h1.style.backgroundColor = clickedColor;
     } else {
         this.style.backgroundColor = "#232323";
-        messageDisplay.textContent = "Try Again"
+        messageDisplay.textContent = "Try Again."
     }
     });
 }
