4ba94988a2d7a50e6afdec0277acb87cf9b2e05c
Andrew Peattie <drewpeattie@hotmail.com>
2020-09-01 10:36:11 +0100

    add selected easy and hard buttons

--- RGB.js
+++ RGB.js
@@ -1,12 +1,6 @@
 var colors = generateRandomColors(6);
-// [
-//     "rgb(255, 0, 0)",
-//     "rgb(255, 255, 0)",
-//     "rgb(0, 255, 0)",
-//     "rgb(0, 255, 255)",
-//     "rgb(0, 0, 255)",
-//     "rgb(255, 0, 255)"
-// ]
+// ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+// "rgb(0, 255, 255)", "rgb(0, 0, 255)", "rgb(255, 0, 255)"]
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -14,6 +8,18 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+});
+
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
 
 resetButton.addEventListener("click", function() {
     // generate all new colors
