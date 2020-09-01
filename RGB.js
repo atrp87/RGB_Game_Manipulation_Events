2d04856c61066dfe27785b67a443048007984d0f
Andrew Peattie <drewpeattie@hotmail.com>
2020-09-01 11:14:15 +0100

    styles

--- RGB.js
+++ RGB.js
@@ -1,7 +1,7 @@
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numSquares);
 // ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
 // "rgb(0, 255, 255)", "rgb(0, 0, 255)", "rgb(255, 0, 255)"]
-
+var numSquares = 6;
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -14,7 +14,8 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function() {
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
-    colors = generateRandomColors(3);
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i = 0; i < squares.length; i++) {
@@ -29,11 +30,19 @@ easyBtn.addEventListener("click", function() {
 hardBtn.addEventListener("click", function() {
     hardBtn.classList.add("selected");
     easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+    }
 });
 
 resetButton.addEventListener("click", function() {
     // generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // pick a new random color from array
     pickedColor = pickColor();
     // change colorDisplay to match picked color
@@ -42,7 +51,7 @@ resetButton.addEventListener("click", function() {
     for(var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
